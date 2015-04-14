752b83d34fb6c5251ddc6c5a0f5445835a7b7624
Team NIM <nim.team@bskyb.com>
2015-04-14 13:53:12 +0100

    Aram + Obrien - rename

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,9 +1,9 @@
 angular.module('pft.controllers')
-    .controller('helloCtrl', function ($scope, $http) {
+    .controller('leadTimeCtrl', function ($scope, $http) {
 
         $scope.master = {};
 
-        $scope.update = function (request) {
+        $scope.post = function (request) {
             $scope.master = angular.copy(request);
             $http.post("rest/leadtime", request).success(function (data) {
                 console.log("suc");
