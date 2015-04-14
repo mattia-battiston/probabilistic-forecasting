de7a5f3e9490bbfd2866618d8b40484a35fa2644
Team NIM <nim.team@bskyb.com>
2015-04-14 10:37:40 +0100

    Aram - adding spring boot application starter

---
+++ src/main/java/com/pft/core/Application.java
@@ -0,0 +1,19 @@
+package com.pft.core;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String... args) {
+
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        builder.run(args);
+    }
+
+}
